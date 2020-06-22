e34443b00d3d5e2e51b690a775ea6058178f163d
Julian Phillips <julian@julianlp.com>
2020-06-21 23:29:47 -0400

    formatting

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-  export default () => (
-    <div className="p-4 shadow rounded bg-white">
-      <h1 className="text-purple-500 leading-normal">Next.js</h1>
-      <p className="text-gray-500">with Tailwind CSS</p>
-    </div>
-  )
-  
+export default () => (
+  <div className="p-4 shadow rounded bg-white">
+    <h1 className="text-purple-500 leading-normal">Next.js</h1>
+    <p className="text-gray-500">with Tailwind CSS</p>
+  </div>
+);
